8b3605f40988af0f10372b572d878cca47d99037
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-04-01 11:53:32 -0500

    New Parts of Python

---
+++ CT_python.py
@@ -0,0 +1,14 @@
+#First Program
+print("Hello World!")
+#New Components
+'Programming'+'Rocks'+'!'
+2+2
+#Variables
+my_int = 1
+my_float = 1.0
+my_bool = False
+my_none = None
+type(my_int)
+type(my_float)
+type(my_bool)
+type(my_none)
--- Python_Elements.py
+++ Python_Elements.py
@@ -1,4 +1,18 @@
-'''Python Language'''
-# Python is a high-level programming language.
-# Python is a General Language
-# Python is an interpreted language
+#Python Language
+'''
+Python is a high-level programming language.
+Python is a General Language
+Python is an interpreted language
+'''
+#Basic Elements
+##Literals
+''' 1, 'abc', 2.0, True/False '''
+##Operators
+''' + / * % ** = == '''
+##<Literals><Operator><Literals>
+print(2 + 2)
+#Types of Error
+'''
+SyntaxError (5 3.0)
+TypeError (1 / "Text")
+'''
