3aab84954c633e6cc7e506d11a3f905dbbd2bddd
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-04-01 11:20:01 -0500

    CT_python.py

--- CT_python.py
+++ CT_python.py
@@ -1,4 +1,4 @@
-#Compute rHistory
+#Computer History
 '''
 "First computer" created by Greeks."
 "In 1801, "Telar de Jacquar."
@@ -8,3 +8,13 @@
 "In 1950, microship, Apple I as example"
 "In XX, Feymann's Architecture, Quantum Computing"
 '''
+#Programming Languages
+'''
+First, define what an algorithm is.
+"An algorithm is a finite list of instructions describing a task, using certain inputs to achieve an output."
+'''
+#Example of algorithm in Python
+def main():
+    print('Hello World!')
+if __name__ == '__main__':
+    main()
