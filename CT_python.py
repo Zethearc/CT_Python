5f169275baab52dd1950abdb486eb9ddc16bac19
Darío Cabezas <81370538+Zethearc@users.noreply.github.com>
2022-04-01 11:05:25 -0500

    New Info

--- CT_python.py
+++ CT_python.py
@@ -1,2 +1,10 @@
-"Hello World!"
-#Lista de literales
+#Compute rHistory
+'''
+"First computer" created by Greeks."
+"In 1801, "Telar de Jacquar."
+"In XIX, Babbage's Analytical Engine, making use of the advances in mechanics (gears) of his time."
+"In XIX, ENIAC by Turing and Alonso."
+"In 1945, EDVAC uses binary system"
+"In 1950, microship, Apple I as example"
+"In XX, Feymann's Architecture, Quantum Computing"
+'''
